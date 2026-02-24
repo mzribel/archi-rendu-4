545217b487df3171a582726a9a5cb4759167094c
RathGate <mariannecorbel@hotmail.fr>
2026-02-24 20:38:30 +0100

    offers: Added usecase methods

--- src/modules/offers/offer.service.ts
+++ src/modules/offers/offer.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { PrismaOfferRepository } from '@modules/offers/repositories/prisma.offer.repository';
 import { IOfferUsecase } from '@modules/offers/usecases/i.offer.usecase';
 
 @Injectable()
 export class OfferService implements IOfferUsecase {
   constructor(private readonly offerRepository: PrismaOfferRepository) {}
+
+  getOffersByCompanyId(companyId:string) {
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  getOfferById(offerId:string){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  createOffer(companyId:string, payload:any){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  updateOffer(offerId:string, payload:any){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  closeOffer(offerId:string){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  deleteOffer(offerId:string){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
+  searchOffer(filters:any){
+    throw new NotImplementedException("Method not implemented.");
+  }
+
 }
--- src/modules/offers/usecases/i.offer.usecase.ts
+++ src/modules/offers/usecases/i.offer.usecase.ts
@@ -1,3 +1,9 @@
 export abstract class IOfferUsecase {
-
+  abstract getOffersByCompanyId(companyId:string);
+  abstract getOfferById(offerId:string);
+  abstract createOffer(companyId:string, payload:any);
+  abstract updateOffer(offerId:string, payload:any);
+  abstract closeOffer(offerId:string);
+  abstract deleteOffer(offerId:string);
+  abstract searchOffer(filters:any);
 }
