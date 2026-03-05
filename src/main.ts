1773f177f01dfe911e8a8c4fb126831cb644d70d
Tony-De-Donato <tonydedo13@gmail.com>
2026-03-05 22:21:02 +0100

    feat: add CORS support to the application

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  app.use(cors())
 
   await app.listen(process.env.PORT ?? 3000);
 }
