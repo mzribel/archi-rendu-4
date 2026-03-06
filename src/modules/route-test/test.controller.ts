25d4a3d1e7b565e645264b7fbd6b40aa52c6622c
RathGate <mariannecorbel@hotmail.fr>
2026-03-06 20:48:42 +0100

    api: added default route for lisibility

--- src/modules/route-test/test.controller.ts
+++ src/modules/route-test/test.controller.ts
@@ -1,40 +1,49 @@
 import { Public, Roles } from '@/common/decorators/roles.decorator';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { User } from '@modules/users/models/user';
 import { CurrentUser } from '@common/decorators/user.decorator';
+import type {Request} from 'express';
 
-@Controller("test")
+@Controller()
 export class TestController {
-
   @Get() @Public()
+  greetings(@Req() req:Request) {
+    return {
+      canard: "🌊🦆🌊",
+      message: "Si vous voyez ce petit canard, c'est que l'API est réveillée et fonctionnelle !",
+      documentation:`${req.protocol}://${req.get('Host')}${req.originalUrl}api/docs`
+    }
+  }
+
+  @Get("test") @Public()
   helloWorld() {
     return "Hello world !"
   }
 
-  @Get("auth")
+  @Get("test/auth")
   testAuth() {
     return "Hello from authenticated route !"
   }
 
-  @Get("admin")
+  @Get("test/admin")
   @Roles("ADMIN")
   testAdminRole() {
     return "Hello from admin route !"
   }
 
-  @Get("student")
+  @Get("test/student")
   @Roles("STUDENT", "ADMIN")
   testStudentRole() {
     return "Hello from student route !"
   }
 
-  @Get("company")
+  @Get("test/company")
   @Roles("COMPANY")
   testCompanyRole() {
     return "Hello from company route !"
   }
 
-  @Get("user")
+  @Get("test/user")
   getUser(@CurrentUser() user:User) {
     return user;
   }
